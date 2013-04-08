669133deb11f4c98f9a5d56b607a0d074930651d
lkorth <luke@lukekorth.com>
2013-04-07 23:38:58 -0500

    Delete contact form

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,6 @@
  *
  * - Toggle
  * - Accordion
- * - Contact Form
  *
  * Copyright 2011, MTD
  * http://themeforest.net/user/MTD
@@ -39,110 +38,6 @@
 					return false;
 				});
 						
-			});
-	
-		},
-		
-		
-		// --- -- - CONTACT FORM PLUGIN - -- --- //
-		elegant_ContactForm	: function(){
-		
-			//Content length validation
-			function validateLength( obj, el, l ){
-				var parent	= el.parents(".formField");
-				if( el.val().length < l ) {
-					parent.addClass("error");
-					return 1;
-				} else {
-					parent.removeClass('error');
-					return 0;
-				}
-			};
-			
-			//email validation
-			function validateEmail( obj, el ){
-				var filter	= /^[a-zA-Z0-9]+[a-zA-Z0-9_.-]+[a-zA-Z0-9_-]+@[a-zA-Z0-9]+[a-zA-Z0-9.-]+[a-zA-Z0-9]+.[a-z]{2,4}$/,
-					parent	= el.parents(".formField");
-				if( filter.test(el.val()) ){
-					parent.removeClass("error");
-					return 0;
-				}else{
-					parent.addClass("error");
-					return 1;
-				}
-			};
-			
-			// Captcha validation
-			function validateCaptcha( obj, el ){
-				var field1		= parseInt( obj.form.find(".captchaField1").text() ),
-					operator	= ( obj.form.find(".captchaField2").text() == "+" ) ? true : false,
-					field3		= parseInt( obj.form.find(".captchaField3").text() ),
-					correct		= operator ? field1+field3 : field1-field3,
-					parent		= el.parents(".captchaField");
-				if( el.val() != correct ){
-					parent.addClass("error");
-					return 1;
-				}else{
-					parent.removeClass('error');
-					return 0;
-				}
-			};
-			
-			return this.submit(function(){
-				
-				var obj	= {
-					form	: $(this),
-					errors	: 0,
-					loader	: $(this).find(".contactLoader"),
-					results	: $(this).find(".contactResults")
-				}
-				
-				obj.results.find("span").hide();
-				obj.loader.fadeIn(200);
-				
-				// Validate required fields
-				obj.form.find(".required").each(function(){
-					if( $(this).hasClass("email") ){
-						obj.errors += validateEmail( obj, $(this) );
-					}else if( $(this).hasClass("captcha") ){
-						obj.errors += validateCaptcha( obj, $(this) );
-					}else{
-						//Must contain at least 3 characters
-						obj.errors += validateLength( obj, $(this), 3 );
-					}
-				});
-
-				//If there are no errors, send the email
-				if(obj.errors === 0){
-					var data	= obj.form.serialize(),
-						URL		= obj.form.attr("action");
-					$.ajax({
-						type	: 'post',
-						url		: URL,
-						data	: data,
-						success	: function(results) {
-							obj.loader.fadeOut(function(){
-								if( /email sent/.test(results) ){
-									// Email sent successfully
-									obj.results.find(".success").fadeIn();
-									obj.form.find(".textField").each(function(){
-										$(this).val("");
-									});
-								} else {
-									obj.loader.fadeOut(200, function(){
-										obj.results.find(".fail").fadeIn();
-									});
-								}
-							});
-						},
-						error: function() {
-							obj.results.find(".fail").fadeIn();
-						}
-					});
-				}else{
-					obj.loader.fadeOut(200);
-				}
-				return false;		
 			});
 	
 		}
